71d487a3d496e83a6cc009f5f54d27eb64ee9d4f
Bhanu Gudheniya <bhanugudheniya1409@gmail.com>
2021-08-04 17:02:40 +0530

    q

---
+++ Ch4_Loop_Control_Instruction/prime.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+    int n, prime = 1;
+
+    printf("Enter a number : ");
+    scanf("%d\n", &n);
+
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            prime = 0;
+            break;
+        }
+    }
+
+    if (prime==0)
+    {
+        printf("This is not a prime number");
+    }
+    else{
+        printf("This is a prime number");
+    }
+    
+}
---
+++ Ch4_Loop_Control_Instruction/repeat8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+    int num, i = 1;
+
+    printf("Enter Interation value : ");
+    scanf("%d\n", &num);
+    
+    while(i<=num){
+        printf("8");
+        i++;
+    }
+}
